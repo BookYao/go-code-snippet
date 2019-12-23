5d1abb4a5381034d0d4f614ce8b3664e95637b65
BookYao <workfile_BookYao@163.com>
2019-12-23 12:28:57 +0800

    1. add tcp echo time test program

---
+++ tcpEchoServer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	for {
+		_, err := io.WriteString(conn, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			loggerPrint("TCP Conn Echo Msg Failed, Maybe Client close!")
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s Port", os.Args[0])
+		os.Exit(0)
+	}
+
+	serviceAddr := "localhost" + ":" + os.Args[1]
+	listener, err := net.Listen("tcp", serviceAddr)
+	if err != nil {
+		log.Println("tcp listen failed!")
+		os.Exit(0)
+	}
+
+	for {
+		connect, err := listener.Accept()
+		if err != nil {
+			loggerPrint("Accept TCP Connect Failed!")
+			continue
+		}
+
+		handleConn(connect)
+	}
+}
